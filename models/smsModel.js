37d0c8e303a8c5a69da431708ff707dadd44280b
solobearus <ivansolobear@gmail.com>
2020-05-05 21:05:24 +0300

    removed console.log

--- models/smsModel.js
+++ models/smsModel.js
@@ -2,14 +2,6 @@ const mysql = require('mysql');
 
 class Database {
     constructor() {
-
-        console.log({
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            database: process.env.DB_DATABASE,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-        });
         
         this.con = mysql.createConnection({
             host: process.env.DB_HOST,
